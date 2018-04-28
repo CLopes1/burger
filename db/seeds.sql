f18034b032f50179ea4a3c8e976a96ee3c07dcf0
Christian Lopes <christianlopes@Christian-Lopess-iMac-2.local>
2018-04-28 15:37:59 -0700

    working app

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ostrich Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bean Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tofu Burger', FALSE);
